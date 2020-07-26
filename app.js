166bc69c4b5531ba661c7811ba785142f9bf9269
diuguide <61917141+diuguide@users.noreply.github.com>
2020-07-25 21:56:44 -0400

    first question set working in CLI

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-const inquirer = require('inquirer');
-const myTeam = [];
-const firstQuestion = [
-    {
-        type: "input",
-        name: ""
+var inquirer = require('inquirer');
 
-    }
-]
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Please enter your name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please enter your id number:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter your email address:",
-    },
+const questionsIntern = {
+    type: "input",
+    name: "school",
+    message: "Please enter your current school:",
+};
+const questionsEngineer = {
+    type: "input",
+    name: "github",
+    message: "Please enter your github username:",
+};
 
-];
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What is your position?",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+            ],
+
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your id number:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address:",
+        },
+
+
+    ])
+    .then(answers => {
+
+    })
+    .catch(error => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else when wrong
+        }
+    });
+
